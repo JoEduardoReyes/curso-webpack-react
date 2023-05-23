f1f61b9115981da970cf227dac4423164a1a7882
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-05-22 19:22:59 -0600

    optimizacion de react

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-	mode: "production",
+	mode: "development",
 	entry: "./src/index.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
-		publicPath: "/",
 	},
 	resolve: {
 		extensions: [".js", ".jsx"],
-		alias: {
-			"@components": path.resolve(__dirname, "src/components/"),
-			"@styles": path.resolve(__dirname, "src/styles/"),
-		},
 	},
 	module: {
 		rules: [
@@ -39,6 +31,11 @@ module.exports = {
 			},
 		],
 	},
+	devServer: {
+		static: path.join(__dirname, "dist"),
+		compress: true,
+		port: 3006,
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./public/index.html",
@@ -47,10 +44,5 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: "[name].css",
 		}),
-		new CleanWebpackPlugin(),
 	],
-	optimization: {
-		minimize: true,
-		minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-	},
 };
